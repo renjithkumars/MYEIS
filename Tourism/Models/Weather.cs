278ceae7c78a617b42ababb12355b1599c4e18d8
renjithkumars <renjith.sk@outlook.com>
2019-04-07 19:13:18 +0100

    new weather

--- Tourism/Models/Weather.cs
+++ Tourism/Models/Weather.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Script.Serialization;
 
@@ -8,5 +9,15 @@ namespace Login.Models
 {
     public class Weather
     {
+        public Object getWeather(string data)
+        {
+            string url = data;
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
     }
 }
+
