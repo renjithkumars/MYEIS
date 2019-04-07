a30ada0a946a4f03038276dac0bae2475fe26210
renjithkumars <renjith.sk@outlook.com>
2019-04-07 19:11:54 +0100

    new class

---
+++ Tourism/Models/Weather.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace Login.Models
+{
+    public class Weather
+    {
+    }
+}
