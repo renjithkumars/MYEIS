faa2cda5921cc29ae2351aaf45af3b48f098f523
renjithkumars <renjith.sk@outlook.com>
2019-04-08 08:23:54 +0100

    weather  controller

--- Tourism/Controllers/WeatherController.cs
+++ Tourism/Controllers/WeatherController.cs
@@ -9,9 +9,22 @@ namespace Login.Controllers
     public class WeatherController : Controller
     {
         // GET: Weather
-        public ActionResult Index()
+        public JsonResult GetWether()
         {
+            string api = "&APPID=e911d33d2a09374dae45a32724aef3fe&unit=imperial";
+
+            string url = string.Concat("http://api.openweathermap.org/data/2.5/weather?q=", "Dublin", "&APPID=e911d33d2a09374dae45a32724aef3fe&unit=imperial");
+            Weather weather = new Weather();
+            return Json(weather.getWeather(url), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Weather()
+        {
+
             return View();
+        }
+        public class Citty
+        {
+            public string City { get; set; }
         }
     }
 }
